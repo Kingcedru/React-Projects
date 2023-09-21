55dc012d4a5876f2e4f076285d02b3a28c4b3e9d
Kingcedru <byishimocedrick@gmail.com>
2023-09-21 15:23:13 +0200

    components

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -3,20 +3,48 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+
+function Header() {
   return (
-    <div className="container">
-    <img src={reactLogo}></img>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-    </div>
+      <header>
+          <nav>
+              <img src={reactLogo} width="40px" />
+              
+          </nav>
+      </header>
   )
 }
 
-export default App
+function Footer() {
+  return (
+      <footer>
+          <small>© 2021 Ziroll development. All rights reserved.</small>
+      </footer>
+  )
+}
+
+function MainContent() {
+  return (
+      <div>
+          <h1>Reasons I'm excited to learn React</h1>
+          <ol>
+              <li>It's a popular library, so I'll be 
+              able to fit in with the cool kids!</li>
+              <li>I'm more likely to get a job as a developer
+              if I know React</li>
+          </ol>
+      </div>
+  )
+}
+
+function Page() {
+  return (
+      <div>
+          <Header />
+          <MainContent />
+          <Footer />
+      </div>
+  )
+}
+
+export default Page
