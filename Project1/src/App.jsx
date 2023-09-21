4eea8eb1e5326cdfb376d9cd5a4ec2926a828d24
Kingcedru <byishimocedrick@gmail.com>
2023-09-21 12:44:33 +0200

    first commit

---
+++ Project1/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+    <img src={reactLogo}></img>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+    </div>
+  )
+}
+
+export default App
