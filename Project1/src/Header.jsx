ebf96593def6ee2e7c186ed9523f96688e3d8612
Kingcedru <byishimocedrick@gmail.com>
2023-09-21 18:50:46 +0200

    final project finished

--- Project1/src/Header.jsx
+++ Project1/src/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return (
         <div className="header">
             <div className="logos">
-                <img src={logo}></img>
+                <img className ='im' src={logo}></img>
                 <h1 className="Htitle">React Facts</h1>
             </div>
             <h2 className="Htitle2">React Course - Project 1</h2>
