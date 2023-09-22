662870fe575f625b5db136f2cad79d4414dba390
Kingcedru <byishimocedrick@gmail.com>
2023-09-22 20:22:22 +0200

    section 3

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -17,9 +17,9 @@ function App() {
     <Content1 />
     <Content2 />
     <div className='flex'>
-      <Content3 img={im1} price={136} title={"Life lessons with Katie Zaferes"}/>
-      <Content3 img={im2} price={125} title={"Learn wedding photography"}/>
-      <Content3 img={im3} price={50} title={"Group Mountain Biking"}/>
+      <Content3  img={im1} price={136} title={"Life lessons with Katie Zaferes"} number={6} country={'USA'} rate={'5.0'}/>
+      <Content3 img={im2} price={125} title={"Learn wedding photography"} number={30} country={'USA'} rate={'5.0'} />
+      <Content3  img={im3} price={50} title={"Group Mountain Biking"} rate={'4.8'} number={2} country={'USA'}/>
     </div>
     
     </div>
--- Airbnb/src/Content3.jsx
+++ Airbnb/src/Content3.jsx
@@ -1,12 +1,16 @@
+import star from './assets/grids/Star 1.png'
 
-
-function Content3({img,price,title}){
+function Content3({img,price,title,number,country,rate}){
     return (
             <div className="grid ml-14 mb-10">
                 <div>
                     <img className="h-64" src={img}></img>
                 </div>
                 <div>
+                    <div className='flex gap-2 items-center'>
+                        <img className= 'w-4 h-4 ' src={star}></img>
+                        <h1 className='font-light'>{rate} <span className='text-slate-500'>({number}) . {country}</span></h1>
+                    </div>
                     <h3 className="text-xs font-light">{title}</h3>
                     <h3 className="text-sm"><span className="font-bold">From ${price}</span> / person</h3>
                 </div>
