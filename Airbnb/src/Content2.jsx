a7240e860114bf1991bfd478f41fe8f711e23113
Kingcedru <byishimocedrick@gmail.com>
2023-09-22 17:09:36 +0200

    section 2

--- Airbnb/src/Content2.jsx
+++ Airbnb/src/Content2.jsx
@@ -2,8 +2,9 @@
 
 function Content2() {
     return (
-        <div>
-            <h1>Online Experiences</h1>
+        <div className="my-20 mx-14 grid gap-9 ">
+            <h1 className="font-bold text-4xl">Online Experiences</h1>
+            <h3 className="font-light text-lg w-80">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h3>
         </div>
     )
 }
