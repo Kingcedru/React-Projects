1dc6a282385d3f9c0b66746e80e2a885773f92fa
Kingcedru <byishimocedrick@gmail.com>
2023-09-22 19:31:07 +0200

    section 3

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -12,14 +12,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='w-3/6 mx-auto rounded-sm shadow-lg'>
     <Navigation />
     <Content1 />
     <Content2 />
-    <Content3 img={im1} price={136} title={"Life lessons with Katie Zaferes"}/>
-    <Content3 img={im2} price={125} title={"Learn wedding photography"}/>
-    <Content3 img={im3} price={50} title={"Group Mountain Biking"}/>
-    </>
+    <div className='flex'>
+      <Content3 img={im1} price={136} title={"Life lessons with Katie Zaferes"}/>
+      <Content3 img={im2} price={125} title={"Learn wedding photography"}/>
+      <Content3 img={im3} price={50} title={"Group Mountain Biking"}/>
+    </div>
+    
+    </div>
   )
 }
 
--- Airbnb/src/Content3.jsx
+++ Airbnb/src/Content3.jsx
@@ -2,17 +2,15 @@
 
 function Content3({img,price,title}){
     return (
-        <div className="flex">
-            <div>
+            <div className="grid ml-14 mb-10">
                 <div>
-                    <img src={img}></img>
+                    <img className="h-64" src={img}></img>
                 </div>
                 <div>
-                    <h3>{title}</h3>
-                    <h3>From $ {price} / person</h3>
+                    <h3 className="text-xs font-light">{title}</h3>
+                    <h3 className="text-sm"><span className="font-bold">From ${price}</span> / person</h3>
                 </div>
             </div>
-        </div>
     )
 }
 export default Content3;
